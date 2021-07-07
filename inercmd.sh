ca02a5e1a84c6d419c451407877d04448de7dbc8
Inertia-OnlineClass <86356597+Inertia-OnlineClass@users.noreply.github.com>
2021-07-07 13:15:19 +0630

    Update inercmd.sh
    Update

--- inercmd.sh
+++ inercmd.sh
@@ -7,4 +7,7 @@ echo ""
 echo ""
 
 printf "midir (dir_name)\t\t : Creating Folder\n"
-printf "cd (dir_name)\t\t : Change Director\n"
+printf "cd (dir_name)\t\t\t : Change Director\n"
+printf "ls\t\t\t\t : View Folders And Files List\n"
+printf "ls -a\t\t\t : View All Files list including hidden files\n"
+printf "ls -la\t\t\t : List All file with Category\n"
