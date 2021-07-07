919529e9194a2946779757a41a222bbfaed9d8f7
Inertia-OnlineClass <86356597+Inertia-OnlineClass@users.noreply.github.com>
2021-07-07 12:57:51 +0630

    Update inercmd.sh
    M

--- inercmd.sh
+++ inercmd.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 clear
 apt install figlet
+clear
 figlet "Basic File"
-printf "midir (dir_name)\t\t : Creating Folder"
+echo ""
+echo ""
+
+printf "midir (dir_name)\t\t : Creating Folder\n"
+printf "cd (dir_name)\t\t : Change Director\n"
